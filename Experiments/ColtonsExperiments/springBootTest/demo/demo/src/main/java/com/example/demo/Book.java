cce4223031ac3a67fe6ea1b9ff543e3b69e20694
C-Glick <crglick@iastate.edu>
2020-08-30 14:11:55 -0500

    Baeldung boot strap tutorial, not building due to database connection

---
+++ Experiments/ColtonsExperiments/springBootTest/demo/demo/src/main/java/com/example/demo/Book.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.data.repository.CrudRepository;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    public long getId() {
+        return id;
+    }
+}
+
+interface BookRepository extends CrudRepository<Book, Long>{
+    List<Book> findByTitle(String title);
+}
