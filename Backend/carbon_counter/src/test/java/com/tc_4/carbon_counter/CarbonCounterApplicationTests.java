9b91740383ae9c3ecdd2b9b814b448ea4e4001a0
C-Glick <crglick@iastate.edu>
2020-09-05 13:31:37 -0500

    Backend spring boot initilization

---
+++ Backend/carbon_counter/src/test/java/com/tc_4/carbon_counter/CarbonCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc_4.carbon_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
