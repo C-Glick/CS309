780438da3a22f7377624e56b54eded101f46f110
Zachary Current <zcurrent@iastate.edu>
2020-09-08 22:26:24 -0500

    Created new activity for login screen and added basic elements

---
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.carbon_counter_front_end;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+        Button loginButton= (Button) findViewById(R.id.buttonLogin);
+    }
+}
