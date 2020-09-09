80caa79386edbbb6169cd57361f16ea35973c53a
Zachary Current <zcurrent@iastate.edu>
2020-09-08 23:06:31 -0500

    modified manifest file to launch login page first, added functionality and notes to the login page

--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/LoginActivity.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.carbon_counter_front_end;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,8 +14,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
+        final TextView username = (TextView) findViewById(R.id.username);
+        final TextView password = (TextView) findViewById(R.id.password);
         Button loginButton= (Button) findViewById(R.id.buttonLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                authenticateUser(username.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+    private void authenticateUser(String username, String password){
+        System.out.println(username + " " + password);
+        //Next, Send HTTP Request to server with the username and password in the body as JSON
+        //This request will return either an empty array or the user we are looking for
+        //We can use this information to tell user what to do next or to switch activities
     }
 }
