d66599e339f1c2daf6148288997cede940ef33e9
C-Glick <crglick@iastate.edu>
2020-09-10 18:58:50 -0500

    test controller deploy
    added user controller and user model classes, testing server connection with test controller

---
+++ Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/controllers/UserController.java
@@ -0,0 +1,8 @@
+package com.tc_4.carbon_counter.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    
+}
---
+++ Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/models/UserModel.java
@@ -0,0 +1,5 @@
+package com.tc_4.carbon_counter.models;
+
+public class UserModel {
+    
+}
