4d2c5759e4b7411213031e526642a12bd53baa1d
Zachary Current <zcurrent@iastate.edu>
2020-09-09 21:46:55 -0500

    I think i have all of the Volley stuff included, but the button doesn't work now. Needs work

--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/LoginActivity.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/LoginActivity.java
@@ -2,12 +2,25 @@ package com.example.carbon_counter_front_end;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.example.carbon_counter_front_end.app.AppController;
+import android.media.MediaCodec;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.toolbox.StringRequest;
+import com.example.carbon_counter_front_end.net_utils.Const;
+
 public class LoginActivity extends AppCompatActivity {
+    private String TAG = LoginActivity.class.getSimpleName();
+    private String tag_string_req = "string_req";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,24 @@ public class LoginActivity extends AppCompatActivity {
 
     private void authenticateUser(String username, String password){
         System.out.println(username + " " + password);
+        String url = "https://b4dde97f-934b-4684-a6f4-5ef4b947e65f.mock.pstmn.io/users";
+
+        StringRequest strReq = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response.toString());
+                System.out.println(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+
         //Next, Send HTTP Request to server with the username and password in the body as JSON
         //This request will return either an empty array or the user we are looking for
         //We can use this information to tell user what to do next or to switch activities
---
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/net_utils/Const.java
@@ -0,0 +1,5 @@
+package com.example.carbon_counter_front_end.net_utils;
+
+public class Const {
+    public static String URL;
+}
