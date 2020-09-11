f1563b1e62e15e7a91e0fddf67a65f82d577f892
C-Glick <crglick@iastate.edu>
2020-09-11 17:17:41 -0500

    can add and view users on the database

--- Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/controllers/UserController.java
+++ Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/controllers/UserController.java
@@ -7,6 +7,8 @@ import com.tc_4.carbon_counter.exceptions.UserNotFoundException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,9 +20,15 @@ public class UserController {
         this.database = database;
     }
 
-    @GetMapping("/getUserInfo/{id}")
+    @GetMapping("/user/{id}")
     public User getUserInfo(@PathVariable Long id){
         return database.findById(id).
         orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user){
+        database.save(user);
+        return user;
     }
 }
--- Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/models/User.java
+++ Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/models/User.java
@@ -2,20 +2,23 @@ package com.tc_4.carbon_counter.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @Column
+    @Column(name="user_name")
     String userName;
 
-    @Column
+    @Column(name="email")
     String email;
 
-    @Column
+    @Column(name="password")
     String password;
 
     public Long getId(){
