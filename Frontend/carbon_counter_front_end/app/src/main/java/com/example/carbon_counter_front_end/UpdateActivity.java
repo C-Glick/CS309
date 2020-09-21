58e8b347611df668a2beb763b772788f53a6c651
Zachary Current <zcurrent@iastate.edu>
2020-09-21 12:30:01 -0500

    error

--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/UpdateActivity.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/UpdateActivity.java
@@ -5,17 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.carbon_counter_front_end.app.AppController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class UpdateActivity extends AppCompatActivity {
+    private String TAG = UpdateActivity.class.getSimpleName();
+    private String tag_json_req = "json_obj_req";
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+
 
         Button backButton = (Button) findViewById(R.id.buttonBack);
         Button viewButton = (Button) findViewById(R.id.buttonView2);
@@ -47,6 +63,27 @@ public class UpdateActivity extends AppCompatActivity {
     }
 
     private void updateInfo(Editable text) {
-        //Send HTTP post with info
+        String url = "http://10.24.227.38:8080/user";
+
+        url += "/" + username;
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                url, null, // IF YOU WANT TO SEND A JSONOBJECT WITH POST THEN PASS IT HERE
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_req);
     }
 }
