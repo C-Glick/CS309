b09477c2739da55354458dbdb3f1e672c8470205
mnmfunk <mnmfunk@iastate.edu>
2020-09-21 01:53:48 -0500

    CreateUser activity and UpdateActivity Completion

--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/CreateUserActivity.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/CreateUserActivity.java
@@ -1,4 +1,14 @@
 package com.example.carbon_counter_front_end;
 
-public class CreateUserActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
 }
