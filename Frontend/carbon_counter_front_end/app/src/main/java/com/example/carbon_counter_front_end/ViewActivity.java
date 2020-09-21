54ef0aabd5c8f53c8bab7479605761ae30916746
Zachary Current <zcurrent@iastate.edu>
2020-09-21 13:20:10 -0500

    needs json array error fixed

--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/ViewActivity.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/ViewActivity.java
@@ -39,7 +39,7 @@ public class ViewActivity extends AppCompatActivity {
      //  final TextView failedUsername = (TextView) findViewById(R.id.failedLogin);
 
 
-        String url = "http://10.24.227.38:8080/stats/addDaily";
+        String url = "http://10.24.227.38:8080/stats";
 
         url += "/" + username;
 
@@ -67,6 +67,7 @@ public class ViewActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 VolleyLog.d(TAG, "Error: " + error.getMessage());
+                System.out.println(error.getMessage());
             }
         });
         // Adding request to request queue
