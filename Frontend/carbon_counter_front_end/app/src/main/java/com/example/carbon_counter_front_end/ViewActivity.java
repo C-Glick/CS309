b5f4a11576744560806d82d0a3066c9e062466a6
mnmfunk <mnmfunk@iastate.edu>
2020-09-28 13:49:13 -0500

    Updating Statistic pages

--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/ViewActivity.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/ViewActivity.java
@@ -5,25 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.carbon_counter_front_end.app.AppController;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class ViewActivity extends AppCompatActivity {
     private String TAG = ViewActivity.class.getSimpleName();
     private String tag_json_get= "json_obj_get";
     private String milesDriven;
     private String username;
-
+    private JSONArray UserInfo = new JSONArray();
+    private JSONObject CurrentData = new JSONObject();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,37 +41,49 @@ public class ViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view);
         Intent intent = getIntent();
         username = intent.getStringExtra("username");
-        getMiles(username);
+
+        try {
+            getMiles(username);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 
 
-    private void getMiles(final String username){
+    private void getMiles(final String username) throws JSONException {
      //  final TextView failedUsername = (TextView) findViewById(R.id.failedLogin);
 
-
-        String url = "http://10.24.227.38:8080/stats/daily";
+        String url = "http://10.24.227.38:8080/stats";
 
         url += "/" + username;
 
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+        JsonArrayRequest jsonObjReq = new JsonArrayRequest(Request.Method.GET,
                 url, null, // IF YOU WANT TO SEND A JSONOBJECT WITH POST THEN PASS IT HERE
-                new Response.Listener<JSONObject>() {
+                new Response.Listener<JSONArray>() {
 
                     @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d(TAG, response.toString());
-                        try {
-                            if (response.get("userName").equals(username)) {
-                                milesDriven = (String) response.get("milesDriven");
-                                TextView mDriven = (TextView) findViewById(R.id.milesDriven);
+                    public void onResponse(JSONArray response) {
+                       // Toast.makeText(ViewActivity.this, "We in the response thing", LENGTH_SHORT).show();
+                        UserInfo = response;
+
+                            try {
+                                CurrentData = UserInfo.getJSONObject(UserInfo.length()-1);
+                                milesDriven = CurrentData.getString("milesDriven");
+
+                                TextView mDriven = findViewById(R.id.milesDriven);
                                 mDriven.setText(milesDriven);
-                            } else {
-                                //Label stating failed username or password
-                               // failedUsername.setText("Invalid username ");
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+
+                        Log.d(TAG, response.toString());
+                        //Label stating failed username or password
+                        // failedUsername.setText("Invalid username ");
+
                     }
                 }, new Response.ErrorListener() {
             @Override
