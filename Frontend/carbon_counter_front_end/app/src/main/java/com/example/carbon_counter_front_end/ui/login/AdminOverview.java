58935a0cc23c3006d69dfe152a04e459b6f94edd
mnmfunk <mnmfunk@iastate.edu>
2020-10-01 21:33:13 -0500

    Created Admin overview, Fixed View Stats url

---
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/ui/login/AdminOverview.java
@@ -0,0 +1,16 @@
+package com.example.carbon_counter_front_end.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.carbon_counter_front_end.R;
+
+public class AdminOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_overview);
+    }
+}
