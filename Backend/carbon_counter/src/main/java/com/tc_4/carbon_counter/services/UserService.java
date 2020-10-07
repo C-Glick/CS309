385975a678b84d6e52fad5db8c826ef5bd129098
C-Glick <crglick@iastate.edu>
2020-10-07 12:48:41 -0500

    update docs

--- Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/services/UserService.java
+++ Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/services/UserService.java
@@ -31,8 +31,8 @@ public class UserService {
      * Get a user object from the database based on their username.
      * Must have at least admin permissions or authenticated as this user.
      * 
-     * @param username user name of the user to get
-     * @return User object 
+     * @param username  user name of the user to get
+     * @return          User object 
      * @throws UserNotFoundException
      */
     public User getUser(String username){
@@ -52,8 +52,8 @@ public class UserService {
     /**
      * Add a user to the database. All user variables are required 
      * 
-     * @param user to add
-     * @return the user that was added to the database
+     * @param user  to add
+     * @return      the user that was added to the database
      */
     public User addUser(User user){
         //encrypt password when saving to the database
@@ -68,7 +68,7 @@ public class UserService {
      * 
      * @param username      The user name of the user to change
      * @param newPassword   The new password to use, pass as plain text, will be encrypted before saving 
-     * @return  boolean, true if the password change was successful
+     * @return              boolean, true if the password change was successful
      * @throws UnauthorizedException if you don't have permission to change this user's password
      */
     public boolean changePassword(String username, String newPassword){
@@ -93,8 +93,8 @@ public class UserService {
      * database, if it does, returns true. If not, throws
      * UserNotFoundException
      * 
-     * @param username the user name to test
-     * @return true if the user does exists
+     * @param username  the user name to test
+     * @return          true if the user does exists
      * 
      * @throws UserNotFoundException if the user does not exist
      */
@@ -110,9 +110,9 @@ public class UserService {
      * Edit the user's information with the passed in information.
      * Must have at least admin permissions or be authenticated as the user to change
      * 
-     * @param username  The current username of the user to edit
+     * @param username      The current username of the user to edit
      * @param userChanges   A user object that *holds only the changes to be made* null variables will be unchanged
-     * @return
+     * @return              the new user after edits have been made
      */
     public User editUser(String username, User userChanges){
         User user = getUser(username);
