5b35727cee070c832fad8664ebe78c178fee3a73
Zachary Current <zcurrent@iastate.edu>
2020-10-08 14:24:28 -0500

    removing login files from template wayyy earlier

--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/data/view/LoginActivity.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/data/view/LoginActivity.java
@@ -1,38 +1,18 @@
 package com.example.carbon_counter_front_end.data.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.carbon_counter_front_end.R;
-import com.example.carbon_counter_front_end.app.AppController;
-import com.example.carbon_counter_front_end.data.model.AppDatabase;
 import com.example.carbon_counter_front_end.data.model.IVolleyListener;
-import com.example.carbon_counter_front_end.data.model.InsertDataThread;
-import com.example.carbon_counter_front_end.data.model.LoggedInUser;
 import com.example.carbon_counter_front_end.data.model.RequestServerForService;
-import com.example.carbon_counter_front_end.data.model.User;
 import com.example.carbon_counter_front_end.data.model.UserInformation;
 import com.example.carbon_counter_front_end.data.presenter.Presenter;
 
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.content.Intent;
-
-
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView failedLogin = (TextView) findViewById(R.id.failedLogin);
