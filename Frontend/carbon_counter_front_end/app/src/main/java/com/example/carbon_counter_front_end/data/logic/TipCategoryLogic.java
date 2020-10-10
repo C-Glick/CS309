b8fb586d2316b0a3cdebb2b091d0cb6fe3da0ac0
Zachary Current <zcurrent@iastate.edu>
2020-10-09 22:45:29 -0500

    finished - tips category page now works

--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/data/logic/TipCategoryLogic.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/data/logic/TipCategoryLogic.java
@@ -10,11 +10,18 @@ import com.example.carbon_counter_front_end.data.view.TipCategoryActivity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Stack;
+
+
 public class TipCategoryLogic {
     private RequestServerForService model;
     private TipCategoryActivity view;
     private Context context;
     private int milesDriven;
+    private int water;
+    private int power;
+    private int meat;
+    private int garbage;
 
     public TipCategoryLogic(TipCategoryActivity view, Context context){
         this.view = view;
@@ -34,6 +41,10 @@ public class TipCategoryLogic {
     public void getStats(JSONObject response) {
         try {
             milesDriven = response.getInt("milesDriven");
+            water = response.getInt("water");
+            power = response.getInt("power");
+            meat = response.getInt("meat");
+            garbage = response.getInt("garbage");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -43,12 +54,51 @@ public class TipCategoryLogic {
         return milesDriven;
     }
 
-    public void setLayout(JSONObject response) {
+    public Stack<Character> setLayout(JSONObject response) {
         getStats(response);
 
+        final int milesIndex = 0;
+        final int waterIndex = 1;
+        final int powerIndex = 2;
+        final int garabageIndex = 3;
+
+        Stack<Character> recommendStack = new Stack<Character>();
+        Stack<Character> allStack = new Stack<Character>();
+
+
+
         if(milesDriven > 100){
-            setRecommendedEmissions();
+            recommendStack.push('m');
+        } else {
+            allStack.push('m');
         }
+
+        if(water > 100){
+            recommendStack.push('w');
+        } else {
+            allStack.push('w');
+        }
+
+        if(power > 100){
+            recommendStack.push('p');
+        } else {
+            allStack.push('p');
+        }
+
+        if(garbage > 100){
+            recommendStack.push('g');
+        } else {
+            allStack.push('g');
+        }
+
+        allStack.push('a');
+
+        for(int i = 0; i < recommendStack.size(); i++){
+            allStack.push(recommendStack.pop());
+        }
+
+        return allStack;
+
     }
 
     public void setRecommendedEmissions() {
--- Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/data/view/TipCategoryActivity.java
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/data/view/TipCategoryActivity.java
@@ -28,6 +28,7 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class TipCategoryActivity extends AppCompatActivity {
     private String TAG = ViewActivity.class.getSimpleName();
@@ -40,17 +41,59 @@ public class TipCategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_category);
 
-
-        Button viewEmissions = (Button) findViewById(R.id.buttonEmissions);
-        Button viewWater = (Button) findViewById(R.id.buttonWater);
-        Button viewWaste = (Button) findViewById(R.id.buttonWaste);
-        Button viewEnergy = (Button) findViewById(R.id.buttonEnergy);
+        final TextView recommended = (TextView) findViewById(R.id.textViewRecommended);
+        final TextView all = (TextView) findViewById(R.id.textViewAll);
+        final Button viewEmissions = (Button) findViewById(R.id.buttonEmissions);
+        final Button viewWater = (Button) findViewById(R.id.buttonWater);
+        final Button viewWaste = (Button) findViewById(R.id.buttonWaste);
+        final Button viewEnergy = (Button) findViewById(R.id.buttonEnergy);
 
         final TipCategoryLogic tipCategoryLogic = new TipCategoryLogic(this, getApplicationContext());
         tipCategoryLogic.setModel(new RequestServerForService(getApplicationContext(), new IVolleyListener() {
             @Override
             public void onSuccess(JSONObject response) {
-                tipCategoryLogic.setLayout(response);
+                Stack<Character> allStack = tipCategoryLogic.setLayout(response);
+
+                Button last = null;
+                int marginTop = 110;
+                int allStackSize = allStack.size();
+
+                for(int i = 0; i < allStackSize; i++){
+                    if(last != null){
+                        marginTop = last.getTop() + 200;
+                    }
+                    char toCompare = allStack.pop();
+                    System.out.println(toCompare);
+                    switch (toCompare){
+                        case 'm':
+                            viewEmissions.setTop(marginTop);
+                            viewEmissions.setBottom(viewEmissions.getTop() + 175);
+                            last = viewEmissions;
+                            break;
+                        case 'w':
+                            viewWater.setTop(marginTop);
+                            viewWater.setBottom(viewWater.getTop() + 175);
+                            last = viewWater;
+                            break;
+                        case 'p':
+                            viewEnergy.setTop(marginTop);
+                            viewEnergy.setBottom(viewEnergy.getTop() + 175);
+                            last = viewEnergy;
+                            break;
+                        case 'g':
+                            viewWaste.setTop(marginTop);
+                            viewWaste.setBottom(viewWaste.getTop() + 175);
+                            last = viewWaste;
+                            break;
+                        case 'a':
+                            all.setTop(marginTop);
+                            last = null;
+                            marginTop = all.getTop() + 80;
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
 
             @Override
