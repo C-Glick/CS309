83b17972c30d759511dc0bddcae7b267bc9b34c0
C-Glick <crglick@iastate.edu>
2020-10-11 23:07:48 -0500

    wrote user tests

--- Backend/carbon_counter/src/test/java/com/tc_4/carbon_counter/CarbonCounterApplicationTest.java
+++ Backend/carbon_counter/src/test/java/com/tc_4/carbon_counter/CarbonCounterApplicationTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootTest
-class CarbonCounterApplicationTests {
+class CarbonCounterApplicationTest {
 
 	@Test
 	void contextLoads() {
