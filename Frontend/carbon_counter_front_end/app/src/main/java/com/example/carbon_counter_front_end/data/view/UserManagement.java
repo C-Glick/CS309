2ea6e38ace80b2efed494315ccdfbf037a36a77d
mnmfunk <mnmfunk@iastate.edu>
2020-10-15 10:22:53 -0500

    created UpdateStatsLogic class

---
+++ Frontend/carbon_counter_front_end/app/src/main/java/com/example/carbon_counter_front_end/data/view/UserManagement.java
@@ -0,0 +1,13 @@
+package com.example.carbon_counter_front_end.data.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_management);
+    }
+}
