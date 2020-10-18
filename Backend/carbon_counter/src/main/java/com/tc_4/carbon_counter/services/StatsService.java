53ae2bfa4d43cb23a1184250d8e62f7bbb9bf371
C-Glick <crglick@iastate.edu>
2020-10-18 14:47:59 -0500

    wrote stats service tests
    - removed user service dependency from stats service
    - refactored from userName to username in daily stats and stats database
    - added test constructor to DailyStats model

--- Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/services/StatsService.java
+++ Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/services/StatsService.java
@@ -22,52 +22,43 @@ public class StatsService {
     @Autowired
     private DailyStatsDatabase dailyStatsDatabase;
 
-    @Autowired
-    private UserService userService;
-
     /**
      * Return the all daily statistics for the given user name.
      * 
-     * @param userName user name of the user to find stats for.
+     * @param username user name of the user to find stats for.
      * @return  a list of DailyStats object
      * @throws UserNotFoundException
      */
-    public List<DailyStats> getUserDailyStats(String userName){
-        //check that user exists
-        userService.doesUserExist(userName);
-        return dailyStatsDatabase.findByUserName(userName);
+    public List<DailyStats> getUserDailyStats(String username){
+        return dailyStatsDatabase.findByUsername(username);
     }
 
     /**
      * Return the user's stats that correspond to the date given
      * 
-     * @param userName user name of user to find stats for.
+     * @param username user name of user to find stats for.
      * @param date the date the stats were submitted 
      * @return returns 0 or 1 dailyStats entry
      */
-    public Optional<DailyStats> getUserDailyStatsByDate(String userName, LocalDate date){
-        //check that user exists
-        userService.doesUserExist(userName);
-        return dailyStatsDatabase.findTopByUserNameAndDateOrderByIdDesc(userName, date);
+    public Optional<DailyStats> getUserDailyStatsByDate(String username, LocalDate date){
+        return dailyStatsDatabase.findTopByUsernameAndDateOrderByIdDesc(username, date);
     }
 
     /**
      * Returns a list of all daily sta  ts form the last month
      * for the given user.
      * 
-     * @param userName user name of user to find stats for.
+     * @param username user name of user to find stats for.
      * @return A list of DailyStats object
      */
-    public List<DailyStats> getLastMonthUserDailyStats(String userName){
-        //check that user exists
-        userService.doesUserExist(userName);
+    public List<DailyStats> getLastMonthUserDailyStats(String username){
         LocalDate oneMonthAgo = LocalDate.now().minusMonths(1);
-        return dailyStatsDatabase.findByUserNameAndDateGreaterThanOrderByDateAsc(userName, oneMonthAgo);
+        return dailyStatsDatabase.findByUsernameAndDateGreaterThanOrderByDateAsc(username, oneMonthAgo);
     }
 
     /**
      * Add stats to the database.
-     * Required fields: userName
+     * Required fields: username
      * If an entry already exists for this user today,
      * it will be overwritten by this new entry.
      * 
@@ -75,12 +66,9 @@ public class StatsService {
      * @return the resulting statistics after adding to the database
      * may contain additional details.
      */
-    public DailyStats addDailyStats(DailyStats stats){
-        //check that user exists
-        userService.doesUserExist(stats.getUserName());
-        
+    public DailyStats addDailyStats(DailyStats stats){        
         //check if an entry already exists for this user today
-        Optional<DailyStats> oldEntry = getUserDailyStatsByDate(stats.getUserName(), LocalDate.now());
+        Optional<DailyStats> oldEntry = getUserDailyStatsByDate(stats.getUsername(), LocalDate.now());
         if(oldEntry.isEmpty()){
             dailyStatsDatabase.save(stats);
             return stats;
