fab0996badc60ba789bc49d6c11e3d980d65e129
C-Glick <crglick@iastate.edu>
2020-10-24 16:16:08 -0500

    completed most of the news API need to handle image posting

--- Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/controllers/NewsController.java
+++ Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/controllers/NewsController.java
@@ -1,35 +1,146 @@
 package com.tc_4.carbon_counter.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+
+import com.tc_4.carbon_counter.exceptions.CarbonFileNotFoundException;
+import com.tc_4.carbon_counter.models.News;
+import com.tc_4.carbon_counter.services.NewsService;
 
 
 @RestController
 public class NewsController {
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @Autowired
+    NewsService newsService;
     
+    /**
+     * Get a stored image by its name.
+     * Will first search the class path of the project for the file,
+     * if not found will then search the file system of the server.
+     */
     @GetMapping("/image/{fileName}")
     public void getImage(HttpServletResponse response, @PathVariable String fileName) throws IOException {
         
         Resource imgFile;
-        try{
-            //file system file (should be server file system, temporarily my machine)
-            imgFile = new FileSystemResource("C:/Users/The_Pheonix/git/309_Project/Backend/carbon_counter/src/main/resources/images/" + fileName);
-        } catch(Exception e){
-            //class path file
-            imgFile = new ClassPathResource("/images/" + fileName);
+        //classpath, stored within the jar file
+        imgFile = resourceLoader.getResource("classpath:" + fileName);
+
+        if(!imgFile.exists()){
+            //file system, location on server to pull images from
+            imgFile = resourceLoader.getResource("file:C:/Users/The_Pheonix/git/309_Project/Backend/carbon_counter/src/main/resources/" + fileName);
         }
 
-        response.setContentType(MediaType.IMAGE_PNG_VALUE);
-        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+        if(imgFile.exists()){
+            response.setContentType(MediaType.IMAGE_PNG_VALUE);
+            StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+        }else{
+            throw new CarbonFileNotFoundException(fileName);
+        }
+    }
+
+    //TODO: save images
+     /**
+     * Get a stored image by its name.
+     * Will first search the class path of the project for the file,
+     * if not found will then search the file system of the server.
+     */
+    @PostMapping("/image/{fileName}")
+    public void saveImage(HttpServletResponse response, @PathVariable String fileName) throws IOException {
+        
+        Resource imgFile;
+        //classpath, stored within the jar file
+        imgFile = resourceLoader.getResource("classpath:" + fileName);
+
+        if(!imgFile.exists()){
+            //file system, location on server to pull images from
+            imgFile = resourceLoader.getResource("file:C:/Users/The_Pheonix/git/309_Project/Backend/carbon_counter/src/main/resources/" + fileName);
+        }
+
+        if(imgFile.exists()){
+            response.setContentType(MediaType.IMAGE_PNG_VALUE);
+            StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+        }else{
+            throw new CarbonFileNotFoundException(fileName);
+        }
+    }
+
+    /**
+     * Add a news item to the database.
+     * Required fields: title, imageTitle, link, body
+     * Optional fields: date(defaults to today)
+     * 
+     * @param news The news item to add to the database, passed in as a JSON object.
+     * @return The news object that was just added as a JSON object.
+     */
+    @PostMapping("/news/add")
+    public News addNews(@RequestBody News news){
+        return newsService.addNews(news);
+    }
+
+    /**
+     * Find a news item by its title.
+     * If no item is found, returns null
+     * 
+     * @param title The title to search by, passed in as a path variable
+     * @return A news object as a JSON object or null
+     */
+    @GetMapping("/news/{title}")
+    public News getNewsByTitle(@PathVariable String title){
+        return newsService.getNewsByTitle(title);
+    }
+
+    /**
+     * Get all news objects that have a date after the date passed in.
+     * List is sorted by date past to present.
+     * 
+     * @param date The date to search by, passed as a path variable in the from of "yyyy-MM-dd"
+     * @return A list of news objects, (can be empty) as a JSON list
+     */
+    @GetMapping("/news/after/{date}")
+    public List<News> getNewsAfterDate(@PathVariable String date){
+        return newsService.getNewsAfterDate(LocalDate.parse(date));
+    }
+
+    /**
+     * Get the most recent n news objects in the database.
+     * If less than n items exist in the database, returns as many
+     * as possible.
+     * List is sorted by date past to present.
+     * 
+     * @param n the number of news objects to get passed in as a path variable.
+     * @return A list of news objects that is up to n in size as a JSON list.
+     */
+    @GetMapping("/news/last/{n}")
+    public List<News> getLastNNews(@PathVariable int n){
+        return newsService.getLastNNews(n);
+    }
+
+    /**
+     * Get all news objects in the database.
+     * List is sorted by date past to present.
+     * 
+     * @return A list of news objects as a JSON list
+     */
+    @GetMapping("/news/all")
+    public List<News> getAllNews(){
+        return newsService.getAllNews();
     }
 }
---
+++ Backend/carbon_counter/src/main/java/com/tc_4/carbon_counter/services/NewsService.java
@@ -0,0 +1,85 @@
+package com.tc_4.carbon_counter.services;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.tc_4.carbon_counter.databases.NewsDatabase;
+import com.tc_4.carbon_counter.models.News;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewsService {
+
+    @Autowired
+    NewsDatabase newsDatabase;
+
+    /**
+     * Add news objects to the database.
+     * 
+     * @param news The news to add
+     * @return The News object
+     */
+    public News addNews(News news) {
+        if (news.getDate() == null) {
+            news.setDate(LocalDate.now());
+        }
+        return newsDatabase.save(news);
+    }
+
+    /**
+     * Retrieve a news object by its title.
+     * Returns null if not news object is found.
+     * 
+     * @param title
+     * @return The news object.
+     */
+    public News getNewsByTitle(String title) {
+        Optional<News> item = newsDatabase.findByTitle(title);
+        if(item.isPresent()){
+            return item.get();
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * get a list of news objects that happened after a certain date
+     * List is sorted by date past to present.
+     * 
+     * @param date
+     * @return a list of news objects
+     */
+    public List<News> getNewsAfterDate(LocalDate date) {
+        return newsDatabase.findByDateGreaterThanOrderByDateAsc(date);
+    }
+
+    /**
+     * Get the last n news items. If n is greater than
+     * the number of news items in the database, will
+     * return less than n items in the list.
+     * List is sorted by date past to present.
+     * 
+     * @param n the number of news items to get
+     * @return a list of news items up to n in size
+     */
+    public List<News> getLastNNews(int n) {
+        List<News> all = newsDatabase.findAllByOrderByDateAsc();
+        List<News> result = new ArrayList<News>();
+        for (int i = 0; i < n && i < all.size(); i++) {
+            result.add(all.get(i));
+        }
+        return result;
+    }
+
+    /**
+     * @return all news objects stored in the database
+     * List is sorted by date past to present.
+     */
+    public List<News> getAllNews(){
+        return newsDatabase.findAllByOrderByDateAsc();
+    }
+}
